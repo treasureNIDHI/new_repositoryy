51c9c8f552e64038367fb83b9a9b80a0a524c1d1
Nidhi <nidhi.jagatpura@gmail.com>
2022-07-27 11:50:27 +0530

    adding division fuction

---
+++ def.py
@@ -0,0 +1,19 @@
+def sum_(x,y):
+    z=x+y
+    return z
+def diff(x,y):
+    z=abs(x-y)
+    return z
+def sum_g(x,y):
+    return (x*y)
+enter=input("enter 'sum or diff or sum_g'")
+n=int(input("enter a digit"))
+m=int(input("enter another digit"))
+if enter=='sum':
+    print(sum_(n,m))
+elif enter=="diff":
+    print(diff(n,m))
+elif enter=="sum_g":
+    print(sum_g(n,m))
+
+    
