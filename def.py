d260c8ae72ae7a5508673ca9ac570571aed6f117
Tejas <tejasbhakar002@gmail.com>
2022-07-31 16:28:57 +0530

    new changes

--- def.py
+++ def.py
@@ -3,15 +3,15 @@ def sum_(x,y):
     #z=x+y
     return (x+y)
 def diff(x,y):
-    z=abs(x-y)
-    return z
+    #z=abs(x-y)
+    return (abs(x-y))
 def sum_g(x,y):
     return (x*y)
 def power(x,y):
     return (x**y)
 def square(x):
     return (x**2)
-enter=input("enter 'sum or diff or sum_g'")
+enter=input("enter 'sum or diff or sum_g or power or square'")
 n=int(input("enter a digit"))
 m=int(input("enter another digit"))
 if enter=='sum':
@@ -20,6 +20,9 @@ elif enter=="diff":
     print(diff(n,m))
 elif enter=="sum_g":
     print(sum_g(n,m))
-"adding a new line"
-
+elif enter=="power":
+    print(power(n,m))
+elif enter=="square":
+    a=int(input("enter the number"))
+    print(square(a))
   
